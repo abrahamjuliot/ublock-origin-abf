8921975195abb6264f36baefe1126e806492c16e
Abraham <abrahamjuliot@gmail.com>
2020-06-15 23:57:08 -0700

    Lie detection onboard and resistance

--- audio.js
+++ audio.js
@@ -74,6 +74,7 @@
         computeFrequencyData(float32Arr)
         return
     }
+    
 	
     function redefine(root) {
         Object.defineProperties(
@@ -86,6 +87,7 @@
                 }
             }
         )
+        	
         Object.defineProperties(
 			root.AnalyserNode.prototype, {
                 'getByteFrequencyData': {
@@ -96,6 +98,36 @@
                 }
             }
     	)
+    	
+    	// Resist lie detection 
+    	function setApiName(api, name) {
+        	Object.defineProperty(api, 'name', { writable: true })
+        	api.name = name
+        }
+    	
+    	setApiName(root.AudioBuffer.prototype.getChannelData, 'getChannelData')
+    	setApiName(root.AudioBuffer.prototype.copyFromChannel, 'copyFromChannel')
+    	setApiName(root.AnalyserNode.prototype.getByteFrequencyData, 'getByteFrequencyData')
+    	setApiName(root.AnalyserNode.prototype.getFloatFrequencyData, 'getFloatFrequencyData')
+    	
+    	const library = {
+    		getChannelData: 'getChannelData',
+    		copyFromChannel: 'copyFromChannel',
+    		getByteFrequencyData: 'getByteFrequencyData',
+    		getFloatFrequencyData: 'getFloatFrequencyData'
+    		
+    	}
+    	const { toString: fnToStr } = Function.prototype
+		function toString() {
+			const name = this.name
+			if (name === library[name]) {
+				return `function ${library[name]}() { [native code] }`
+			}
+			return fnToStr.apply(this, arguments)
+		}
+		root.Function.prototype.toString = toString
+		root.Function.prototype.toString.toString = () => 'function toString() { [native code] }'
+    	
     }
 
     redefine(window)
@@ -107,3 +139,58 @@
     })
     //audioTest()
 })()
+
+
+// Detect Lie
+function hasLiedAPI(api, name) {
+	const hashMini = str => {
+	    const json = `${JSON.stringify(str)}`
+	    let i, len, hash = 0x811c9dc5
+	    for (i = 0, len = json.length; i < len; i++) {
+	        hash = Math.imul(31, hash) + json.charCodeAt(i) | 0
+	    }
+	    return ('0000000' + (hash >>> 0).toString(16)).substr(-8)
+	}
+	const native = (x) => `function ${x}() { [native code] }`
+	let lieTypes = []
+	let fingerprint = ''
+	
+	// detect attempts to rewrite Function string conversion APIs
+	const fnToStr = Function.prototype.toString
+	const fnToLStr = Function.prototype.toLocaleString
+	const fnStr = String
+	const fnStringify = JSON.stringify
+	if (fnToStr != native('toString')) { lieTypes.push({ fnToStr }) }
+	if (fnToLStr != native('toLocaleString')) { lieTypes.push({ fnToLStr }) }
+	if (fnStr != native('String')) { lieTypes.push({ fnStr }) }
+	if (fnStringify != native('stringify')) { lieTypes.push({ fnStringify }) }
+	
+	// detect attempts to rename the API and/or rewrite string conversion APIs on this API object
+	const { name: apiName, toString: apiToString, toLocaleString: apiToLocaleString } = api
+	if (apiName != name) { lieTypes.push({ apiName }) }
+	if (apiToString !== fnToStr) { lieTypes.push({ apiToString }) }
+	if (apiToLocaleString !== fnToLStr) { lieTypes.push({ apiToLocaleString }) }
+	
+	// collect string conversion result
+	const result = ''+api
+	
+	// fingerprint result if it does not match native code
+	if (result != native(name)) { fingerprint = result }
+	
+	//console.log({ lieTypes, fingerprint })
+	
+	return {
+		lied: lieTypes.length || fingerprint ? true : false,
+		hash: hashMini({ lieTypes, fingerprint })
+	}
+}
+
+function lieDetect(api, name) {
+	const { lied, hash } = hasLiedAPI(api, name)
+	return lied && console.log(`API Lie Detected: ${hash}`)
+}
+
+lieDetect(AudioBuffer.prototype.getChannelData, 'getChannelData')
+lieDetect(AudioBuffer.prototype.copyFromChannel, 'copyFromChannel')
+lieDetect(AnalyserNode.prototype.getByteFrequencyData, 'getByteFrequencyData')
+lieDetect(AnalyserNode.prototype.getFloatFrequencyData, 'getFloatFrequencyData')
