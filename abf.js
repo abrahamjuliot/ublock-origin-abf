bf04f54cf9b3045a38a9158453ee36ac6fe6514e
Abraham <abrahamjuliot@gmail.com>
2020-07-17 02:36:57 +0000

    clearColor

--- abf.js
+++ abf.js
@@ -73,15 +73,13 @@
 			const shadowColor = randomRGBA()
 			const strokeStyle = randomRGBA()
 			const font = randomFont()
-			const widthOffset = rand(-10, 10)
-			const heightOffset = rand(-10, 10)
+			const clearColor = [...Array(4)].map(() => Math.random().toFixed(1))
 			return {
 				fillStyle,
 				shadowColor,
 				strokeStyle,
 				font,
-				widthOffset,
-				heightOffset
+				clearColor
 			}
 		}
 		const canvasContextComputed = canvas()
@@ -172,11 +170,9 @@
 		const context = nativeGetContext.apply(canvas, [canvas._contextType])
 		if (context) {
 			const {
-				widthOffset,
-				heightOffset
+				clearColor
 			} = canvasContextComputed
-			context.width += widthOffset
-			context.height += heightOffset
+			context.clearColor(...clearColor)
 		}
 		return context
 	}
